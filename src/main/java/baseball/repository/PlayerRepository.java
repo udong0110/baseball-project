85a31bb906a30a4941a9b6375902409f0dad8d9c
udong0110 <uuudong00@gmail.com>
2026-06-16 17:38:09 +0900

    feat: 선수저장소 클래스 생성

--- src/main/java/baseball/repository/PlayerRepository.java
+++ src/main/java/baseball/repository/PlayerRepository.java
@@ -1,4 +1,30 @@
 package baseball.repository;
 
+import baseball.domain.stat.*;
+import baseball.domain.player.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PlayerRepository {
+
+    private Map<Player, PlayerStat> playerRepositoryMap = new HashMap<>();
+
+
+    public void save(Player player, PlayerStat playerStat) {
+        playerRepositoryMap.put(player, playerStat);
+    }
+
+    public PlayerStat findByPlayer(Player player) {
+        for (Player playerKey : playerRepositoryMap.keySet()) {
+            if (playerKey.equals(player)) {
+                return playerRepositoryMap.get(player);
+            }
+        }
+        return null;
+    }
+
+    public Map<Player, PlayerStat> findAll() {
+        return playerRepositoryMap;
+    }
 }
