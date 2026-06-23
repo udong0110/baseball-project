3a578baa80e62c423c605d3eea854d51d5b086de
udong0110 <uuudong00@gmail.com>
2026-06-23 16:45:57 +0900

    refactor: 기존의 Hitter, Pitcher클래스에 존재하던 약점,강점, 구종정보를  Stat클래스로 옮겨서 데이터 분리

--- src/main/java/baseball/domain/stat/HitterStat.java
+++ src/main/java/baseball/domain/stat/HitterStat.java
@@ -1,17 +1,34 @@
 package baseball.domain.stat;
 
+import baseball.domain.player.Zone;
+import baseball.exception.InvalidPitchMetricException;
+
 public class HitterStat extends PlayerStat {
 
     private double ops;
     private double avg;
 
-    public HitterStat(double ops, double avg) {
+    private Zone hitterpowerZone;
+    private Zone hitterWeakZone;
+
+
+    public HitterStat(double ops, double avg, Zone hitterpowerZone, Zone hitterWeakZone) {
         if (avg < 0.0 || avg > 1.0) {
-            throw new IllegalArgumentException("타율 수치 오류(0.0 ~ 1.0 사이여야 함): " + avg);
+            throw new InvalidPitchMetricException("타율 수치 오류(0.0 ~ 1.0 사이여야 함): " + avg);
         }
         if (ops < 0.0 || ops > 2.0) {
-            throw new IllegalArgumentException("OPS 수치 오류(음수 불가): " + ops);
+            throw new InvalidPitchMetricException("OPS 수치 오류(음수 불가): " + ops);
         }
+
+        if ((hitterpowerZone == null) || (hitterpowerZone.getZoneNumber() < 1) || (hitterpowerZone.getZoneNumber() > 9)) {
+            throw new IllegalArgumentException("존 입력실패.");
+        }
+        if ((hitterWeakZone == null) || (hitterWeakZone.getZoneNumber() < 1) || (hitterWeakZone.getZoneNumber() > 9)) {
+            throw new IllegalArgumentException("존 입력실패.");
+        }
+
+        this.hitterpowerZone = hitterpowerZone;
+        this.hitterWeakZone = hitterWeakZone;
 
         this.ops = ops;
         this.avg = avg;
@@ -26,9 +43,19 @@ public class HitterStat extends PlayerStat {
         return avg;
     }
 
+    public Zone getHitterpowerZone() {
+        return hitterpowerZone;
+    }
+
+    public Zone getHitterWeakZone() {
+        return hitterWeakZone;
+    }
+
     @Override
     public String toString() {
         return "ops=" + ops +
-                ", avg=" + avg;
+                ", avg=" + avg+
+                ", 강점=" + hitterpowerZone +
+                ", 약점=" + hitterWeakZone ;
     }
 }
--- src/main/java/baseball/domain/stat/PitcherStat.java
+++ src/main/java/baseball/domain/stat/PitcherStat.java
@@ -1,18 +1,44 @@
 package baseball.domain.stat;
 
+import baseball.domain.player.Pitch;
+import baseball.domain.player.PitchType;
+import baseball.domain.player.PlayerPosition;
+import baseball.exception.InvalidPitchMetricException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PitcherStat extends PlayerStat {
 
     private double era;
+    private final Map<PitchType, Pitch> pitch;
+    private Pitch strongPitch;
 
-    public PitcherStat(double era) {
+    public PitcherStat(double era, Map<PitchType,Pitch> pitch, Pitch strongPitch) {
         if (era > 100.0 || era < 0) {
-            throw new IllegalArgumentException("era는 음수이거나 100이 넘어갈 수 없습니다.");
+            throw new InvalidPitchMetricException("era는 음수이거나 100이 넘어갈 수 없습니다.");
         }
+        if (pitch == null || strongPitch == null) {
+            throw new IllegalArgumentException("구종 입력실패");
+        }
+        this.pitch = pitch;
+        this.strongPitch = strongPitch;
         this.era = era;
     }
 
     @Override
     public String toString() {
-        return "era=" + era;
+        List<String> pitchList = new ArrayList<>();
+        for (Pitch p : pitch.values()) {
+            pitchList.add(p.toString());
+        }
+        String pitchInfo = String.join(", ", pitchList);
+
+
+        return "era=" + era +
+                ", 구종= {" + pitchInfo +"}"+
+                ", 핵심구종=" + strongPitch;
     }
 }
