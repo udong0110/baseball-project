b6d2dae2a9a2e928995cbda72834a736c4f885da
udong0110 <uuudong00@gmail.com>
2026-06-23 16:44:41 +0900

    refactor: Hitter, Pitcher클래스에는 선수에 대한 기본 정보만을 다루게 변경 (구종이나, 존의 정보를 스탯으로 변경)

--- src/main/java/baseball/domain/player/Hitter.java
+++ src/main/java/baseball/domain/player/Hitter.java
@@ -2,35 +2,19 @@ package baseball.domain.player;
 
 public class Hitter extends Player {
 
-    private Zone hitterpowerZone;
-    private Zone hitterWeakZone;
+    public Hitter(String name, Team team, HandType hand, PlayerPosition playerPosition) {
+        super(name, team, hand, playerPosition);
 
-    public Hitter(String name, Team team, HandType hand, Zone hitterpowerZone, Zone hitterWeakZone) {
-        super(name, team, hand);
-        if ((hitterpowerZone == null) || (hitterpowerZone.getZoneNumber() < 1) || (hitterpowerZone.getZoneNumber() > 9)) {
-            throw new IllegalArgumentException("존 입력실패.");
-        }
-        if ((hitterWeakZone == null) || (hitterWeakZone.getZoneNumber() < 1) || (hitterWeakZone.getZoneNumber() > 9)) {
-            throw new IllegalArgumentException("존 입력실패.");
-        }
-        this.hitterpowerZone = hitterpowerZone;
-        this.hitterWeakZone = hitterWeakZone;
     }
 
-    public Zone getHitterpowerZone() {
-        return hitterpowerZone;
-    }
-
-    public Zone getHitterWeakZone() {
-        return hitterWeakZone;
-    }
 
     @Override
     public String toString() {
         return "[타자] " +
                 "이름: " + getName() +
-                ", 팀: " + getTeam()+
-                ", 강점 존=" + hitterpowerZone +
-                ", 약점 존=" + hitterWeakZone ;
+                ", 팀: " + getTeam() +
+                ", 타석: " + getHandType() +
+                ", 포지션: " + getPlayerPosition()
+                ;
     }
 }
--- src/main/java/baseball/domain/player/Pitcher.java
+++ src/main/java/baseball/domain/player/Pitcher.java
@@ -1,36 +1,25 @@
 package baseball.domain.player;
 
-import java.util.List;
+import java.util.Map;
 
 public class Pitcher extends Player {
 
-    private List<PitchType> pitchType;
-    private PitchType strongPitchType;
 
+    public Pitcher(String name, Team team, HandType hand, PlayerPosition playerPosition) {
+        super(name, team, hand, playerPosition);
 
-    public Pitcher(String name, Team team, HandType hand, List<PitchType> pitchType, PitchType strongPitchType) {
-        super(name, team,hand);
-        if (pitchType == null || strongPitchType == null) {
-            throw new IllegalArgumentException("구종 입력실패");
-        }
-        this.pitchType = pitchType;
-        this.strongPitchType = strongPitchType;
     }
 
-    public List<PitchType> getPitchType() {
-        return pitchType;
-    }
 
-    public PitchType getStrongPitchType() {
-        return strongPitchType;
-    }
+
 
     @Override
     public String toString() {
         return "[투수] " +
                 "이름: " + getName() +
-                ", 팀: " + getTeam()+
-                ", 구종=" + pitchType +
-                ", 주무기=" + strongPitchType;
+                ", 팀: " + getTeam() +
+                ", 투구: " + getHandType() +
+                ", 포지션: " + getPlayerPosition();
+
     }
 }
