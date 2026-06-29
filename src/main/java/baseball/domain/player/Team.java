5272e15e7fe48d40620ab3276dc8d7ff549f5ecc
udong0110 <uuudong00@gmail.com>
2026-06-29 14:23:37 +0900

    refactor: 팀이름을 한글,영어로 검색할 수 있게 변경

--- src/main/java/baseball/domain/player/Team.java
+++ src/main/java/baseball/domain/player/Team.java
@@ -1,31 +1,33 @@
 package baseball.domain.player;
 
 public enum     Team {
-    LOTTE("롯데 자이언츠"),
-    HANHWA("한화 이글스"),
-    DOOSAN("두산 베어스"),
-    KIWOOM("키움 히어로즈"),
-    LG("엘지 트윈스"),
-    SAMSUNG("삼성 라이온즈"),
-    KT("케이티 위즈"),
-    SSG("SSG 랜더스"),
-    NC("엔씨 다이노스"),
-    KIA("기아 타이거즈");
+    LOTTE("롯데 자이언츠","LOTTE"),
+    HANHWA("한화 이글스","HANWHA"),
+    DOOSAN("두산 베어스","DOOSAN"),
+    KIWOOM("키움 히어로즈","KIWOOM"),
+    LG("엘지 트윈스","LG"),
+    SAMSUNG("삼성 라이온즈","SAMSUNG"),
+    KT("케이티 위즈","KT"),
+    SSG("SSG 랜더스","SSG"),
+    NC("엔씨 다이노스","NC"),
+    KIA("기아 타이거즈","KIA");
 
-    private final String teamName;
+    private final String teamKoreanName;
+    private final String teamEnglishName;
 
 
-    Team(String teamName) {
-        this.teamName = teamName;
+    Team(String teamName, String teamEnglishName) {
+        this.teamKoreanName = teamName;
+        this.teamEnglishName = teamEnglishName;
     }
 
-    public String getTeamName() {
-        return teamName;
+    public String getTeamKoreanName() {
+        return teamKoreanName;
     }
 
     public static Team findTeamByKorean(String inputTeamName) {
         for (Team team : Team.values()) {
-            if (team.teamName.equals(inputTeamName)) {
+            if (team.teamKoreanName.equals(inputTeamName)|| team.teamEnglishName.equalsIgnoreCase(inputTeamName)) {
                 return team;
             }
         }
@@ -34,6 +36,6 @@ public enum     Team {
 
     @Override
     public String toString() {
-        return teamName;
+        return teamKoreanName;
     }
 }
