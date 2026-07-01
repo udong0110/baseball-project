b848e273b6d5b45e172167af0f7a875c12d61c64
udong0110 <uuudong00@gmail.com>
2026-07-01 12:43:00 +0900

    refactor: PitchType클래스 평균속도,평균rpm을 둬서 분석기에서 비교 용이하게 변경

--- src/main/java/baseball/domain/player/PitchType.java
+++ src/main/java/baseball/domain/player/PitchType.java
@@ -2,21 +2,34 @@ package baseball.domain.player;
 
 public enum PitchType {
 
-    FASTBALL("직구"),
-    CUTTER("커터"),
-    CURVEBALL("커브"),
-    SINKER("싱커"),
-    SLIDER("슬라이더"),
-    CHANGEUP("체인지업"),
-    FORKBALL("포크"),
-    TWOSEAM("투심"),
-    KNUCKLEBALL("너클");
+    FASTBALL("직구", 145.0, 2250.0),
+    CUTTER("커터", 139.5, 2350.0),
+    CURVEBALL("커브", 118.0, 2600.0),
+    SINKER("싱커", 138.5, 2000.0),
+    SLIDER("슬라이더", 133.5, 2470.0),
+    CHANGEUP("체인지업", 131.5, 1700.0),
+    FORKBALL("포크", 130.0, 1250.0),
+    TWOSEAM("투심", 142.0, 2180.0),
+    KNUCKLEBALL("너클", 105.0, 300.0);
 
 
     private final String pitchType;
+    private final double avgSpeed;
+    private final double avgRpm;
 
-    PitchType(String pitchType) {
+
+    PitchType(String pitchType, double avgSpeed, double avgRpm) {
         this.pitchType = pitchType;
+        this.avgSpeed = avgSpeed;
+        this.avgRpm = avgRpm;
+    }
+
+    public double getAvgSpeed() {
+        return avgSpeed;
+    }
+
+    public double getAvgRpm() {
+        return avgRpm;
     }
 
     public String getPitchType() {
