721aabbed6f27f4018bd8938812def648bfc4b79
udong0110 <uuudong00@gmail.com>
2026-07-01 11:50:15 +0900

    chore: HandPitcherType 클래스에서 스위치투수 삭제(kbo역사상 존재x)

--- src/main/java/baseball/domain/player/HandPitcherType.java
+++ src/main/java/baseball/domain/player/HandPitcherType.java
@@ -2,8 +2,8 @@ package baseball.domain.player;
 
 public enum HandPitcherType {
     LEFT("좌투"),
-    RIGHT("우투"),
-    SWITCH("양투");
+    RIGHT("우투");
+    // 스위치투수는 kbo역사상 존재하지 않았기 때문에 생략
 
     private final String description;
 
