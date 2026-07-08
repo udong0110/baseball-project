7aca068346d865e7db464bd7b74401e96424b93f
udong0110 <uuudong00@gmail.com>
2026-07-08 17:38:51 +0900

    refactor: Player클래스 equals메서드에서 getClass를 사용해서 Pitcher,Hitter가 같은 투타겸업 선수가 들어와도 Map에 들어가지게 리팩토링

--- src/main/java/baseball/domain/player/Player.java
+++ src/main/java/baseball/domain/player/Player.java
@@ -23,15 +23,13 @@ public abstract class Player {
     public boolean equals(Object o) {
         if (this == o) return true;
         // Hitter,Picther 클래스로 선수를 만들기때문에 Player 클래스로 형변환 시켜줘야함
-        if (!(o instanceof Player)) return false;
+        if (!(o instanceof Player)|| getClass() != o.getClass()) return false;  // getClass를 사용해서 Pitcher,Hitter가 같은 투타겸업 선수가 들어와도 Map에 들어가짐
 
         Player player = (Player) o;
         return Objects.equals(name, player.name) &&
                 team == player.team &&
                 backNumber == player.backNumber;
-                // 이게 Player계열은 검증하지만 Hitter인지 Pitcher인지는 검증해주지 않는다
-                // 따라서 같은팀 같은 이름 같은 등번호인 타자 투수를 저장소에 넣으려면 하나가 나머지를 덮어버릴 수 있다
-                // 투타겸업인 선수가 나오면 리팩토링
+
     }
 
 
